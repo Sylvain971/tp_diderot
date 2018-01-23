3fcea5d49b9098b5c4b8219d81a948d40455a486
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-01-23 15:29:15 +0100

    4 méthodes

--- app.rb
+++ app.rb
@@ -1,3 +1,20 @@
 10.times do
 	puts "Hello World"	
 end
+
+i = 1
+for i in 1..10 do
+	puts "Hello World"
+end
+
+tab = ["Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World", "Hello World",]
+
+tab.each do |item|
+	puts item
+end
+
+i = 1
+while i < 11 do
+	puts "Hello World"
+	i+=1
+end
